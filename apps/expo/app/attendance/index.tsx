a7817597a2b369242eeeaab31584cdd568104181
bachlythanh <ltb1002.edmail@gmail.com>
2023-02-27 22:48:28 +0700

    more of date

--- apps/expo/app/attendance/index.tsx
+++ apps/expo/app/attendance/index.tsx
@@ -1,11 +1,16 @@
+import { Moment } from "moment";
+import { useState } from "react";
 import { Text, View } from "react-native";
-import CalendarPicker from "react-native-calendar-picker";
+import DateRangePicker from "../../src/components/DateRangePicker";
 
 const AttendanceHome = () => {
+  const [date, setDate] = useState<Moment | null>(null);
+
   return (
     <View>
       <Text>Attendance Home</Text>
-      <CalendarPicker onDateChange={() => "HOHO"} allowRangeSelection={true} />
+      <Text>{date?.toString() ?? ""}</Text>
+      <DateRangePicker onChange={(date) => setDate(date)} />
     </View>
   );
 };
---
+++ apps/expo/src/components/DatePicker.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
+
+const DatePicker = () => {
+  return (
+    <View>
+      <CalendarPicker />
+    </View>
+  );
+};
+
+export default DatePicker;
