af2638e15f7e51a4c4b380c3ced7320ee3eb41ff
bachlythanh <ltb1002.edmail@gmail.com>
2023-02-27 22:20:04 +0700

    init draft dev

---
+++ apps/expo/app/attendance/statistics.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AttendanceStatistics = () => {
+  return (
+    <View>
+      <Text>Attendance Statistics</Text>
+    </View>
+  );
+};
+
+export default AttendanceStatistics;
