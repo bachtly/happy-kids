98a2d732ca56793810638c42ca7cace708f46750
Nguyen Vo <voxuannguyen2001@gmail.com>
2023-03-08 16:00:31 +0700

    remove converting to base64 for userId

--- packages/api/src/service/login-service.ts
+++ packages/api/src/service/login-service.ts
@@ -29,7 +29,7 @@ class LoginService {
           return {
             status: LoginStatus.enum.Success,
             message: "login succeeded",
-            userId: resp.id.toString("base64")
+            userId: resp.id
           };
         }
       })
