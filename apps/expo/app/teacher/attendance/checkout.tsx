4ba04b841dc958eba2f551d7cfe3ddc5ffd66c12
edwardly1002 <ltb1002.edmail@gmail.com>
2023-03-12 11:58:50 +0700

    for teacher

---
+++ apps/expo/app/teacher/attendance/checkout.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { ScrollView, View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+import StudentItem from "../../../src/components/attendance/StudentItem";
+import { StudentModel } from "../../../src/models/AttendanceModels";
+import { api } from "../../../src/utils/api";
+import { useAuthContext } from "../../../src/utils/auth-context-provider";
+
+const AttendanceCheckout = () => {
+  // properties
+  const { colors } = useTheme();
+  const { classId } = useAuthContext();
+
+  const [filter, setFilter] = useState({
+    searchStr: "",
+    status: ""
+  });
+
+  // data
+  const [studentList, setStudentList] = useState<StudentModel[]>([]);
+  const attMutation = api.attendance.getStudentList.useMutation({
+    onSuccess: (resp) => setStudentList(resp.students)
+  });
+
+  // update list when search criterias change
+  useEffect(() => {
+    classId &&
+      attMutation.mutate({
+        classId: classId
+      });
+  }, [classId]);
+
+  return (
+    <View className={"flex-1 bg-white px-2"}>
+      <ScrollView>
+        <View className={"pt-3"}>
+          {studentList && studentList.length > 0 ? (
+            studentList.map((item, key) => <StudentItem key={key} {...item} />)
+          ) : (
+            <View className={"mt-5 mb-10"}>
+              <Text
+                className={"text-center"}
+                variant={"titleLarge"}
+                style={{ color: colors.gray }}
+              >
+                Không có dữ liệu để hiển thị
+              </Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default AttendanceCheckout;
