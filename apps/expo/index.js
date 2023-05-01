a7612698c053d2502109ffb6a2800a12511f3d7b
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2023-05-01 20:09:38 +0700

    Fix eas update does not make app auto update on device (lack of package expo-update). (#68)
    When add expo-update, fail build because index.js does not exist, fixed it as well

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,3 +1,5 @@
 // This is the entry point for the Expo app.
 // It is responsible for setting up the router, root component and loading pages.
-import "expo-router/entry";
+// @ts-ignore
+// eslint-disable-next-line no-undef
+_ = require("expo-router/entry");
